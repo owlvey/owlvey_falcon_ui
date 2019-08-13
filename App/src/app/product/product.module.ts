5cc6cdfecad6abdf753d73dadb34a3f4398ac07a
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2019-08-13 16:49:52 -0500

    Product wip

--- App/src/app/product/product.module.ts
+++ App/src/app/product/product.module.ts
@@ -6,7 +6,7 @@ import { ProductRoutingModule } from './product.routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [CreateProductComponent, ListProductComponent],
+  declarations: [ListProductComponent],
   imports: [CommonModule, SharedModule, ProductRoutingModule]
 })
 export class ProductModule { }
