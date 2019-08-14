a267b9223765eb6a4a3d9a7212cc8c1498b5bdd7
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2019-08-13 19:49:00 -0500

    add

--- src/app/pages/source/source.module.ts
+++ src/app/pages/source/source.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateSourceComponent } from './create-source/create-source.component';
@@ -33,29 +32,3 @@ import {
 export class SourceModule { }
 
 
-
-=======
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { CreateSourceComponent } from "./create-source/create-source.component";
-import { ListSourceComponent } from "./list-source/list-source.component";
-import { SourceRoutingModule } from "./source.routing.module";
-import {
-  NbCardModule,
-  NbIconModule,
-  NbInputModule,
-  NbTreeGridModule
-} from "@nebular/theme";
-import { Ng2SmartTableModule } from "ng2-smart-table";
-
-@NgModule({
-  declarations: [CreateSourceComponent, ListSourceComponent],
-  imports: [
-    CommonModule,
-    SourceRoutingModule,
-    Ng2SmartTableModule,
-    NbCardModule
-  ]
-})
-export class SourceModule {}
->>>>>>> 06135fc8b2059ae4ddb56b851e645ae5cccf2d3e
--- src/app/pages/source/source.routing.module.ts
+++ src/app/pages/source/source.routing.module.ts
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetailSourceComponent } from './detail-source/detail-source.component';
 import { ListSourceComponent } from './list-source/list-source.component';
 
-
-
 const routes: Routes = [
   { path: '', component: ListSourceComponent },
   { path: ':sourceId', component: DetailSourceComponent}  
 ];
-=======
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { ListSourceComponent } from "./list-source/list-source.component";
-
-const routes: Routes = [{ path: "", component: ListSourceComponent }];
->>>>>>> 06135fc8b2059ae4ddb56b851e645ae5cccf2d3e
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
