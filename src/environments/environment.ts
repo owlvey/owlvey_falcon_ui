74a39e5fc7f0eb30cd0401d8bc456014278cd7a5
Fabrizio Daniel Cordova Holguin <fabrizio1793@gmail.com>
2019-08-21 18:25:17 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   applicationName: "Falcon.com",
   clientId: "pswebadmin",
-  authority: "http://localhost:5001/",
-  api: "http://localhost:5001/"
+  authority: "http://localhost:64370/",
+  api: "http://localhost:64370/"
 };
