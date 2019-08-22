6ee284d38f82b6d72f3a39672ca4dd7d205a43b7
Gustavo Rabanal <gustavo@ieholding.com>
2019-08-22 11:09:31 -0500

    update

--- src/app/@core/utils/env.service.ts
+++ src/app/@core/utils/env.service.ts
@@ -7,8 +7,9 @@ export class EnvironmentService {
   }
 
   getUrl(baseUrl: string, type: string): string {
-    if (type && type === "docker") {
-      return "http://" + window.location.host + "/";
+    if (type && type === "docker")
+    {
+      return "http://" + window.location.hostname + ":45001/";
     }
     return baseUrl;
   }
