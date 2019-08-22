b5680d9cb45b51c8be555c325740f50b66520139
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2019-08-21 21:13:31 -0500

    Changes for creating a product

--- src/app/pages/product/product.routing.module.ts
+++ src/app/pages/product/product.routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListProductComponent } from './list-product/list-product.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 
 const routes: Routes = [
   { path: '', component: ListProductComponent },
-  { path: 'detail', component: DetailProductComponent}  
+  { path: 'detail', component: DetailProductComponent},
+  { path: 'create', component: CreateProductComponent}
 ];
 
 @NgModule({
@@ -14,4 +16,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class ProductRoutingModule { }
- 
