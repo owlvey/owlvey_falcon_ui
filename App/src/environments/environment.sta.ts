c0b07ecbd2bc1011f7ea5329c821bc361f865567
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2019-09-08 19:32:52 -0500

    Deleting old App folder

--- App/src/environments/environment.sta.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  applicationName: 'Falcon.com',
-  clientId: 'pswebadmin',
-  authority: 'https://falcon.com/',
-  api: 'https://falcon.com/',
-};
--- App/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  applicationName: "Falcon.com",
-  clientId: "pswebadmin",
-  authority: "https://localhost:5001/",
-  api: "https://localhost:5001/"
-};
