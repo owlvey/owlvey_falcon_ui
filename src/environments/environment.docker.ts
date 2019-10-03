db4f33dcd15558d1fc1a01ed2fc351b23fa2dc18
Felipe Pacora <martin.pacora@gmail.com>
2019-10-02 21:42:28 -0500

    fix

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,9 +1,13 @@
 export const environment = {
   production: false,
-  applicationName: 'Falcon.com',
-  clientId: 'pswebadmin',
-  authority: 'http://authority:45002/',
-  api: 'http://api:45001/',
-  type: 'docker',
-  showDevMenu: false
+  applicationName: "Falcon.com",
+  clientId: "pswebadmin",
+  authority: "http://authority:45002/",
+  api: "http://api:45001/",
+  type: "docker",
+  showDevMenu: false,
+  githubFalconURL: "https://github.com",
+  facebookFalconURL: "https://facebook.com",
+  twitterFalconURL: "http://twitter.com",
+  linkedinFalconURL: "https://linkedin.com"
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,13 @@
 export const environment = {
   production: true,
-  applicationName: 'Falcon.com',
-  clientId: 'pswebadmin',
-  authority: 'https://falcon.com/',
-  api: 'https://falcon.com/',
-  type: 'regular',
-  showDevMenu: false
+  applicationName: "Falcon.com",
+  clientId: "pswebadmin",
+  authority: "https://falcon.com/",
+  api: "https://falcon.com/",
+  type: "regular",
+  showDevMenu: false,
+  githubFalconURL: "https://github.com",
+  facebookFalconURL: "https://facebook.com",
+  twitterFalconURL: "http://twitter.com",
+  linkedinFalconURL: "https://linkedin.com"
 };
