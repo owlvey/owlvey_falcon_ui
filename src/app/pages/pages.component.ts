1435ef15c2f496eb0f06d24b2897a2d629be0111
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2019-11-04 22:49:28 -0500

    add

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -42,7 +42,7 @@ export class PagesComponent implements OnInit {
   }
 
   ngOnInit(){    
-    this.themeService.changeTheme("dark"); // change theme
+    //this.themeService.changeTheme("dark"); // change theme
     this.menuService.onItemClick().subscribe((e) => {      
       
       const currentCustomer =  parseInt(this.getParameterByName("customerId"));
