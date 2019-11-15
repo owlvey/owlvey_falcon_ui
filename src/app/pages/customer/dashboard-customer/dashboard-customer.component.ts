18afaa2541f0f7d97bff5740b0be1628fe0900ba
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2019-11-15 08:39:58 -0500

    add

--- src/app/pages/customer/dashboard-customer/dashboard-customer.component.ts
+++ src/app/pages/customer/dashboard-customer/dashboard-customer.component.ts
@@ -66,6 +66,13 @@ export class DashboardCustomerComponent extends BaseComponent implements OnDestr
         this.segments = data.categories;
         this.products = data.products.map(c=>{
 
+          if (c.effectiveness < 1){
+            c.textClass = "text-danger";
+          }
+          else {
+            c.textClass = "text-success";            
+          }
+
           const targetData = c.groups.map(d => {
             const temp = {
                 value: [ d.index, d.count],   
