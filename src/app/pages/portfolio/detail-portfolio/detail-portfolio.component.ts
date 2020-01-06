ecfd6925dfc788955e5667760fbaf9b1c47b3a71
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-01-05 21:00:31 -0500

    add

--- src/app/pages/portfolio/detail-portfolio/detail-portfolio.component.ts
+++ src/app/pages/portfolio/detail-portfolio/detail-portfolio.component.ts
@@ -88,8 +88,7 @@ export class DetailPortfolioComponent implements OnInit {
         filter: false,
         width: '3em',
         editable: false,        
-      },        
-                 
+      },                         
     },
   };
   /*
