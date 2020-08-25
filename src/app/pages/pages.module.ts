639b167aaf1ef41c2e02c19809a37402a7132e5e
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-08-25 09:32:14 -0500

    add

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,8 +3,6 @@ import { NbMenuModule, NbIconModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 
@@ -24,8 +22,6 @@ import { SyncModule } from './sync/sync.module';
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
-    ECommerceModule,
     MiscellaneousModule,
     SourceModule,
     CustomerModule,
